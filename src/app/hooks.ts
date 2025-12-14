289cbef64c78a204416202a3a800a56a5d993692
Madhusudan K S <madhusudan.ks1712@gmail.com>
2025-12-14 13:30:33 +0530

    Delete src directory

--- src/app/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux";
-import type { RootState, AppDispatch } from "./Store";   
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/features/auth/authSelectors.ts
+++
@@ -1,6 +0,0 @@
-import { RootState } from "../../app/Store";
-
-export const selectAuthLoading = (state: RootState) => state.auth.loading;
-export const selectIsAuthenticated = (state: RootState) => state.auth.isAuthenticated;
-export const selectRedirecting = (state: RootState) => state.auth.redirecting;
-export const selectUser = (state: RootState) => state.auth.user;
